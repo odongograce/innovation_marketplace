7e654ed9f22cbc6b03cb1589780cc37570156708
Castrokimaru <castrokimaru@gmail.com>
2026-02-02 08:58:14 +0300

    next-auth module declaration

---
+++ frontend/types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      name?: string | null
+      email?: string | null
+      image?: string | null
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    id: string
+    name?: string | null
+    email?: string | null
+    username?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    name?: string
+  }
+}
+
